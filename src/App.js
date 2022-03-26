1b0f220f226d62c98db1fe082d521a16eba4215e
unknown <eric199900@gmail.com>
2022-03-26 17:35:47 -0400

    Cleaned up directory and made my changes (:

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {makeStyles} from '@material-ui/core/styles';
+import { Box, CssBaseline, Grid } from '@material-ui/core';
+import Header from './components/Header';
+import { blue } from '@material-ui/core/colors';
 
-function App() {
+
+// const useStyles = makeStyles((themes) => ({
+//   root: {
+//      minHeight: '100vh',
+//     backgroundImage: `url(${process.env.PUBLIC_URL + 'background.jpg'})`,
+//     backgroundRepeat:"no-repeat",
+//     backgroundSize: 'cover',
+//      // background: "rgb(232, 241, 250)",
+
+//   },
+// }));
+
+export default function App() {
+  // const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <body>Hello</body>
+    <Grid
+        sx={{
+          bgcolor: '#FCECDD'
+        }}
+        container
+        color={'#FCECDD'}
+        spacing={0}
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+        style={{ minHeight: '100vh' }}
+      >
+        <Grid item xs={3}>
+      <Header />
+      </Grid>
+      </Grid>
+      </>
   );
+
 }
 
-export default App;
+
+
+
+
+
+
+
+
+
+
+
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>This is text between</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,44 @@
+// import React from 'react';
+// import {makeStyles} from '@material-ui/core/styles';
+// import { AppBar, CssBaseline, IconButton, Toolbar } from '@material-ui/core';
+// import MenuIcon from '@mui/icons-material/Menu';
+
+
+
+// const useStyles = makeStyles((themes) => ({
+//   appbar:{
+//       background: 'none',
+//       fontFamily: "Nunito",
+//   },
+//   menuicon:{
+//       color:"#fff", 
+//       fontSize: '1rem',
+//   },
+//   appbarTitle:{
+//     flexGrow: "1",
+//   },
+//   appbarWrapper:{
+//     width: '80%',
+//     margin: '0 auto',
+//   },
+//   colorText:{
+//     color: "#FF6701",
+//   },
+// }));
+
+// export default function Header() {
+//   const classes = useStyles();
+//   return( <div>
+//     <AppBar className={classes.appbar} elevation={0}>
+//         <Toolbar className={classes.appbarWrapper}>
+//             <h1 className={classes.appbarTitle}>
+//                 Challenge<span className={classes.colorText}>Community</span>
+//             </h1>
+//             <IconButton>
+//                 <MenuIcon className={classes.menuicon}></MenuIcon>
+//             </IconButton>
+//         </Toolbar>
+//     </AppBar>
+//   </div>
+//   );
+// }
